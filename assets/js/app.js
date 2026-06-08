dd8b03e798fb0df6142460e9f94a48cce19f9286
Axel Cardozo <axelcardozo369@gmail.com>
2026-06-08 10:40:57 -0300

    se agrego la lista de cards para cada personaje

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,34 @@ const URLPersonajes = "https://thesimpsonsapi.com/api/characters";
 const URLDetalles = "https://thesimpsonsapi.com/api/characters/";
 
 let personajes = [];
+const miContenedor = document.querySelector("#coleccionPersonajes");
+console.log(miContenedor);
+const contenedor = document.querySelector("#coleccionPersonajes");
+
 const obternerMisPersonajes = async () => {
   try {
     const respuesta = await fetch(URLPersonajes);
     const datos = await respuesta.json();
     personajes = datos.results;
+    contenedor.innerHTML = "";
+    personajes.forEach((personaje) => {
+      contenedor.innerHTML += `
+      <div class="card" style="width: 18rem;">
+  <img src="personajes.img" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Título de la tarjeta</h5>
+    <p class="card-text">Un breve texto de ejemplo para mostrar en el título de la tarjeta y componer la mayor parte del contenido de la tarjeta.</p>
+    <a href="#" class="btn btn-primary">Ir a algún lugar</a>
+  </div>
+</div>
+`;
+    });
     console.log(personajes);
   } catch (error) {
     console.log("hay errores axel" + error);
   }
 };
+
+// funcion cargar personajes
+
 document.addEventListener("DOMContentLoaded", obternerMisPersonajes);
