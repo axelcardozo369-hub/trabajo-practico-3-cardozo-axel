7ffb42d24b251c2992ba61b7129f4c159b2a4b40
Axel Cardozo <axelcardozo369@gmail.com>
2026-06-11 22:27:54 -0300

    se agrego la vinculacion con el URL de imagenes de cada personaje y funciones con la estructura html para ir armando bien cada imagen

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 //declaro primero mis URL de los personajes
 const URLPersonajes = "https://thesimpsonsapi.com/api/characters";
 const URLDetalles = "https://thesimpsonsapi.com/api/characters/";
+const URlImagenes = "https://cdn.thesimpsonsapi.com/500";
 
 let personajes = [];
 const miContenedor = document.querySelector("#coleccionPersonajes");
@@ -12,6 +13,21 @@ const obternerMisPersonajes = async () => {
     const respuesta = await fetch(URLPersonajes);
     const datos = await respuesta.json();
     personajes = datos.results;
+    personajes.forEach(function (personaje) {
+      const columna = document.createElement("div");
+      columna.className = "col-md-4";
+      columna.innerHTML = `
+      <div class="card">
+      <img src = "${URlImagenes}${personaje.portrait_path}" class = "card-img-top" alt="${personaje.name}">
+        <div class="card-body">
+            <h5 class="card-title">${personaje.name}</h5>
+            <p class = "card-text"> ${personaje.occupation}</p>
+            <p class = "card-text">${personaje.status}</p>
+        </div>
+    </div>
+      `;
+      miContenedor.appendChild(columna);
+    });
     console.log(personajes);
   } catch (error) {
     console.log("hay errores axel" + error);
